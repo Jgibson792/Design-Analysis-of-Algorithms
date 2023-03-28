56d12e73e351f376d312c0f0757f5419982706e4
Jgibson792 <125433992+Jgibson792@users.noreply.github.com>
2023-03-28 01:08:27 -0500

    Update Project2.java

--- Project2.java
+++ Project2.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Project2 {
 	
 	//Main driver
@@ -9,11 +10,10 @@ public class Project2 {
 		
 	}
 	
-	//Creates a directed graph
 	/* For team mates: 
-	 * addEdge now accepts varags! addEdge(source, destinations...) 
+	 * addEdge now accepts varargs! addEdge(source, destinations...) 
 	 * This graph has prerequisites pointing to post requisites. Change as needed */
-	
+	//Creates and returns directed graph
 	static DirectedGraph createGraph() {
 		DirectedGraph graph = new DirectedGraph();
 
@@ -25,48 +25,48 @@ public class Project2 {
 		graph.addVertex("PHYS 1408"); 	graph.addEdge("MATH 1451", "PHYS 1408");
 		graph.addVertex("ENGL 1302");	graph.addEdge("ENGL 1301", "ENGL 1302");
 		graph.addVertex("CS 2413"); 	graph.addEdge("CS 1412", "CS 2413");
-		graph.addVertex("CS 1382");		graph.addEdge("CS 1412", "CS 1382");
+		graph.addVertex("CS 1382");	graph.addEdge("CS 1412", "CS 1382");
 		graph.addVertex("ECE 2372");	graph.addEdge("MATH 1451", "ECE 2372");
 		graph.addVertex("MATH 2450"); 	graph.addEdge("MATH 1452", "MATH 2450");
 		graph.addVertex("PHYS 2401"); 	graph.addEdge("PHYS 1408", "PHYS 2401");
-		graph.addVertex("CS 2350");		graph.addEdge("CS 1412", "CS 2350"); graph.addEdge("ECE 2372", "CS 2350");
-		graph.addVertex("CS 2365");		graph.addEdge("CS 2413", "CS 2365");
+		graph.addVertex("CS 2350");	graph.addEdge("CS 1412", "CS 2350"); graph.addEdge("ECE 2372", "CS 2350");
+		graph.addVertex("CS 2365");	graph.addEdge("CS 2413", "CS 2365");
 		graph.addVertex("ENGR 2392");
 		graph.addVertex("POLS 1301");
 		graph.addVertex("MATH 2360");	graph.addEdge("MATH 1452", "MATH 2360");
 		graph.addVertex("ENGL 2311");	graph.addEdge("ENGL 1301", "ENGL 2311"); graph.addEdge("ENGL 1302", "ENGL 2311");
-		graph.addVertex("CS 3361");		graph.addEdge("CS 2413", "CS 3361");
-		graph.addVertex("CS 3364");		graph.addEdge("CS 2413", "CS 3364"); graph.addEdge("CS 1382", "CS 3364"); graph.addEdge("MATH 2360", "CS 3364");
+		graph.addVertex("CS 3361");	graph.addEdge("CS 2413", "CS 3361");
+		graph.addVertex("CS 3364");	graph.addEdge("CS 2413", "CS 3364"); graph.addEdge("CS 1382", "CS 3364"); graph.addEdge("MATH 2360", "CS 3364");
 		graph.addVertex("MATH 3342");	graph.addEdge("MATH 2450", "MATH 3342");
 		graph.addVertex("POLS 2306");
-		graph.addVertex("CS 3365");		graph.addEdge("CS 2365", "CS 3365"); graph.addEdge("CS 2413", "CS 3365"); graph.addEdge("MATH 3342", "MATH 3365");
-		graph.addVertex("CS 3375");		graph.addEdge("CS 2350", "CS 3375");
-		graph.addVertex("CS 3383");		graph.addEdge("CS 1382", "CS 3383");
-		graph.addVertex("CS 4365");		graph.addEdge("CS 1382", "CS 4365");
-		graph.addVertex("CS 4352");		graph.addEdge("CS 3364", "CS 4352"); graph.addEdge("CS 3375", "CS 4352");
-		graph.addVertex("CS 4354");		graph.addEdge("CS 3364", "CS 4352"); graph.addEdge("CS 3375", "CS 4352");
-		graph.addVertex("CS 4366");		graph.addEdge("CS 4365", "CS 4366");
+		graph.addVertex("CS 3365");	graph.addEdge("CS 2365", "CS 3365"); graph.addEdge("CS 2413", "CS 3365"); graph.addEdge("MATH 3342", "MATH 3365");
+		graph.addVertex("CS 3375");	graph.addEdge("CS 2350", "CS 3375");
+		graph.addVertex("CS 3383");	graph.addEdge("CS 1382", "CS 3383");
+		graph.addVertex("CS 4365");	graph.addEdge("CS 1382", "CS 4365");
+		graph.addVertex("CS 4352");	graph.addEdge("CS 3364", "CS 4352"); graph.addEdge("CS 3375", "CS 4352");
+		graph.addVertex("CS 4354");	graph.addEdge("CS 3364", "CS 4352"); graph.addEdge("CS 3375", "CS 4352");
+		graph.addVertex("CS 4366");	graph.addEdge("CS 4365", "CS 4366");
 		
 		return graph;
 	}
 	
 	//depthFirstSearch() creates an iterator with all vertices, iterates through it until all vertices are visited
 	static void depthFirstSearch(DirectedGraph graph) {
-		Iterator<DirectedGraph.Vertex> itr = graph.adjacent.keySet().iterator();								//creates iterator for all vertices
-		Set<DirectedGraph.Vertex> visited = new LinkedHashSet<DirectedGraph.Vertex>();										//creates visited = false for all vertices
-		while(itr.hasNext()) {																	//for all vertices
-			if(!visited.contains(itr.next()))													//if not visited
-				explore(graph, itr.next(), visited);											//explore
+		Iterator<DirectedGraph.Vertex> itr = graph.adjacent.keySet().iterator();				//creates iterator for all vertices
+		Set<DirectedGraph.Vertex> visited = new LinkedHashSet<DirectedGraph.Vertex>();				//creates visited = false for all vertices
+		while(itr.hasNext()) {											//for all vertices
+			if(!visited.contains(itr.next()))								//if not visited
+				explore(graph, itr.next(), visited);							//explore
 		}
 	}
 	
-	//explore() explores all vertices in a vertex's adjacency list
+	//explore() explores all vertices adjacent to a vertex (recursively)
 	static void explore(DirectedGraph graph, DirectedGraph.Vertex root, Set<DirectedGraph.Vertex> visited) {
 		if(!visited.contains(root) && graph.getAdjVertices(root.label)!= null)  { 				//if not visited
-			visited.add(root); 																	//visited = true
-			if(graph.getAdjVertices(root.label)!= null) {										//if there are adjacent vertices
-				for (int i = 0; i<graph.getAdjVertices(root.label).size(); i++) {				//visit all vertices in the adjacency list
-					explore(graph, graph.getAdjVertices(root.label).get(i), visited);			//recurs until all vertices have been visited
+			visited.add(root); 										//visited = true
+			if(graph.getAdjVertices(root.label)!= null) {							//if there are adjacent vertices
+				for (int i = 0; i<graph.getAdjVertices(root.label).size(); i++) {			//visit all vertices in the adjacency list
+					explore(graph, graph.getAdjVertices(root.label).get(i), visited);		//recurs until all vertices have been visited
 				}
 			}
 		}
@@ -79,16 +79,16 @@ class DirectedGraph {
 	//Declares a map interface named adjacent
 	public Map<Vertex, List<Vertex>> adjacent;
 
-    //Constructor, initialized adjacent as a hash map object, with a key pointing to a list
+    //Constructor, initializes adjacent as a hash map object
     DirectedGraph() {
         this.adjacent = new HashMap<Vertex, List<Vertex>>();			//vertex, adjacency list
     }
 
     void addVertex(String label) {
-        adjacent.putIfAbsent(new Vertex(label), new ArrayList<>());
+        adjacent.putIfAbsent(new Vertex(label), new ArrayList<>()); 		//Creates a key(Vertex) and maps it to an ArrayList(Adjacent vertices)
     }
 
-    //addEdge(String source, String... destination) adds any number of destinations to the source's adjacency list
+    //addEdge(String source, String... destination) adds any number of destinations to the source's adjacent vertices
     void addEdge(String source, String... destination) {
         Vertex sourceVertex = new Vertex(source);
         for(int i=0;i<destination.length;i++) {
@@ -97,7 +97,7 @@ class DirectedGraph {
         }
     }
 
-    //getAdjacent(String label) takes a label as an input and returns the vertices adjacent to the vertex with that label
+    //getAdjacent(String label) takes a String(Vertex label) as an input and returns the list of adjacent vertices
     List<Vertex> getAdjVertices(String label) {
         return adjacent.get(new Vertex(label));
     }
@@ -128,7 +128,7 @@ class DirectedGraph {
             this.label = label;
         }
         
-        //hashCode() returns a unique hash code for the vertex
+        //hashCode() returns a unique hash code for this Vertex instance
         @Override
         public int hashCode() {
             int result = 1;
@@ -137,7 +137,7 @@ class DirectedGraph {
             return result;
         }
         
-        //equals(Object obj) takes an object to see if it is equal to this vertex
+        //equals(Object obj) takes an object to see if it is equal to this Vertex instance
         @Override
         public boolean equals(Object obj) {
             if (this == obj)
@@ -156,7 +156,7 @@ class DirectedGraph {
             return true;
         }
         
-        //toString() returns the label of the vertex when called
+        //toString() returns the label of the Vertex instance when called
         @Override
         public String toString() {
             return label;
